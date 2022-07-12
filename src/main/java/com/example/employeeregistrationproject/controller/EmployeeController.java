2dad684429b6af9f9affacc09bc63dd6f2ad29e8
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-12 17:52:18 -0600

    added thymeleaf bindings to html

--- src/main/java/com/example/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/example/employeeregistrationproject/controller/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.example.employeeregistrationproject.controller;
 
+import com.example.employeeregistrationproject.bootstrap.DataGenerator;
+import com.example.employeeregistrationproject.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    public String createEmployee() {
-
+    @GetMapping("/register")
+    public String createEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
         return "employee/employee-create";
     }
 }
