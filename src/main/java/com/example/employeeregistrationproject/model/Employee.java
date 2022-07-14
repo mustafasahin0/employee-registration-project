b122ac440c7d31cdd890f952295fec88e18c913e
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-13 19:16:41 -0600

    added validations

--- src/main/java/com/example/employeeregistrationproject/model/Employee.java
+++ src/main/java/com/example/employeeregistrationproject/model/Employee.java
@@ -3,6 +3,10 @@ package com.example.employeeregistrationproject.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,7 +16,12 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
+
+
     private String lastName;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
